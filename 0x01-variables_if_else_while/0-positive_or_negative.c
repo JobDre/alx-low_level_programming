52f06111f5bbe9ef6602d17a44b044a936e46e61
Awiti Job <awitijob811@gmail.com>
2022-09-11 15:25:43 -0700

    negative or positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - print if the umber is positive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+ /* your code goes there */
+if (n > o)
+{
+	printf("%i is positive\n", n);
+}
+else if (n == 0)
+{
+	printf("%i is zero\n", n);
+else if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
+return (0);
